563b578c8d2da8780649083acb40986fc50fd30a
David Kroll <curtain333-code@yahoo.com>
2017-05-01 08:05:29 -0400

    added log viewer

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,18 @@ Route::get('/', function() {
   return ('Welcome to Assignment 4');
 });
 
+
+
+
+
+/**
+* Log viewer
+* (only accessible locally)
+*/
+//if(config('app.env') == 'local') {
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+//}
+
 /* commented out 4-30-2017:
 Route::get('/', function () {
     return view('welcome');
