42f899743a32c74a11f5bf9eff2aca00eb032c61
David Kroll <curtain333-code@yahoo.com>
2017-05-02 12:10:08 -0400

    index method of ItemController now dumps item table data

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Dictionary;
+use App\Item;
 
 class ItemController extends Controller
 {
     public function index() {
+
+        # Get all rows
+        $result = Item::all();
+            dump($result->toArray());
+
         return ('Welcome to ItemController.php and Assignment 4');
+
     } // end of index function
-    
+
 }
