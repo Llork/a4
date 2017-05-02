789d6dcae7e967d4375523cd053ded63f86cda4a
David Kroll <curtain333-code@yahoo.com>
2017-05-02 12:01:05 -0400

    added index method to ItemController

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    public function index() {
+        return ('Welcome to ItemController.php and Assignment 4');
+    } // end of index function
+    
 }
