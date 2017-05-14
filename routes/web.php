fc4d34c85ace454edd4ce7ec23cdc37c549333e6
David Kroll <curtain333-code@yahoo.com>
2017-05-14 15:28:52 -0400

    modified composer.json and web.php to remove log viewer and debugbar from prod, and to remove development(practice) routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| ItemController Web Routes:
+| ItemController Web Routes (an 'item' is a synchronicity):
 |--------------------------------------------------------------------------
 */
 
@@ -38,46 +38,6 @@ Route::get('/delete_dictionary/{id}', 'DictionaryController@deleteDictionary');
 Route::post('/delete_dictionary', 'DictionaryController@reallyDeleteDictionary');
 
 
-/*
-|--------------------------------------------------------------------------
-| Development Web Routes - remove before submitting a4:
-|--------------------------------------------------------------------------
-*/
-Route::get('/dev1', 'DevelopmentController@dev1');
-Route::get('/dev2', 'DevelopmentController@dev2');
-Route::get('/dev3', 'DevelopmentController@dev3');
-Route::get('/dev4', 'DevelopmentController@dev4');
-
-Route::get('/debug', function() {
-	echo '<pre>';
-	echo '<h1>Environment</h1>';
-	echo App::environment().'</h1>';
-	echo '<h1>Debugging?</h1>';
-	if(config('app.debug')) echo "Yes"; else echo "No";
-	echo '<h1>Database Config</h1>';
-    echo 'DB defaultStringLength: '.Illuminate\Database\Schema\Builder::$defaultStringLength;
-    /*
-    The following commented out line will print your MySQL credentials.
-    Uncomment this line only if you're facing difficulties connecting to the database and you
-    need to confirm your credentials.
-    When you're done debugging, comment it back out so you don't accidentally leave it
-    running on your production server, making your credentials public.
-    */
-	//print_r(config('database.connections.mysql'));
-	echo '<h1>Test Database Connection</h1>';
-	try {
-		$results = DB::select('SHOW DATABASES;');
-		echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-		echo "<br><br>Your Databases:<br><br>";
-		print_r($results);
-	}
-	catch (Exception $e) {
-		echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-	}
-	echo '</pre>';
-});
-
-
 /**
 * Drop then create database a4
 * (only accessible locally)
@@ -96,8 +56,8 @@ if(App::environment('local')) {
 
 /**
 * Log viewer
-* (change this to only be accessible locally)
+* (only be accessible locally)
 */
-//if(config('app.env') == 'local') {
+if(config('app.env') == 'local') {
     Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-//}
+}
